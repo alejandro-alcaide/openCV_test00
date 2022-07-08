d7b510dee6226c3d7df48407ef9baa8c076789ec
alejandro-alcaide <alcaide.dev@gmail.com>
2022-07-08 01:26:33 -0500

    shape detection and incremental functionality in tools

--- alx_openCV_tools.py
+++ alx_openCV_tools.py
@@ -43,11 +43,55 @@ def get_contoursA(img_canny, img_original):
         cv2.drawContours(img_original, cnt, -1, (255, 0, 0), 3)
     return img_original
 
+
 def get_contours(img_canny):
-    img_blank = np.zeros_like(img_canny)
+    ih, iw = img_canny.shape
+    img_blank = np.zeros((ih, iw, 3), np.uint8)
+    # img_blank = np.zeros_like(img_canny)
     contours, hierarchy = cv2.findContours(img_canny, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
     for cnt in contours:
         area = cv2.contourArea(cnt)
-        # print("area:", area)
-        cv2.drawContours(img_blank, cnt, -1, (255, 0, 0), 3)
+
+        # making sure the lines are not noise. areas must be bigger than 500 units
+        if area > 500:
+            # draw the contours (vertices) on the blank canvas
+            cv2.drawContours(img_blank, cnt, -1, (255, 255, 255), 3)
+            perimeter = cv2.arcLength(cnt, True)
+            # get corner points
+            corner_points = cv2.approxPolyDP(cnt, .02 * perimeter, True)
+            # get each figures' corner count
+            corners_count = len(corner_points)
+
+            # this gives x0, y0, xf, yf of each figure
+            x, y, w, h = cv2.boundingRect(corner_points)
+            # with x, y, w, h draw boxes around each figure from point x, y to x+w, y+h
+            # from these, info like coordinates, total width, height, center of the figure can be gathered
+            cv2.rectangle(img_blank, (x, y), (x + w, y + h), (255, 0, 255), 3)
+            figure_type = None
+            if corners_count == 3:
+                figure_type = "Triangle"
+            elif corners_count == 4:
+                aspect_ratio = float(w / h)
+                print(f"4 corners aspect ratio: {aspect_ratio}")
+                # we have a pct of error
+                if aspect_ratio > 0.98 and aspect_ratio < 1.03:
+                    figure_type = "Square"
+                else:
+                    figure_type = "Rectangle"
+            elif corners_count == 8:
+                figure_type = "Circle"
+
+            # write in the center of our figure
+            cv2.putText(
+                img_blank,
+                figure_type,
+                (x + (w // 2) - 10, y + (h // 2) - 10),
+                cv2.FONT_HERSHEY_COMPLEX,
+                .35,
+                (255, 0, 255),
+                1
+            )
+
+            print(f"area: {area}, perimeter: {perimeter}, corners_count: {corners_count}, x:{x}, y:{y}, w: {w}, h:{h},"
+                  + f" figure_type: {figure_type}")
     return img_blank
--- ch08_detect_shapes.py
+++ ch08_detect_shapes.py
@@ -5,12 +5,16 @@ img = cv2.imread("resources/shapes.png")
 
 # convert to grayscale
 img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+# blur it
 img_blur = cv2.GaussianBlur(img_gray, (7, 7), 1)
+# get it's edges
 img_canny = cv2.Canny(img_blur, 50, 50)
 
+# call get_contours to get the vertices
 img_contours = get_contours(img_canny)
 
-img_stack = stackImages(.5, ([img, img_gray, img_blur], [img, img_canny, img_contours]))
+img_stack = stackImages(.7, ([img, img_gray, img_blur], [img, img_canny, img_contours]))
 
 cv2.imshow("shapes", img_stack)
+cv2.imshow("contours only", img_contours)
 cv2.waitKey(0)
